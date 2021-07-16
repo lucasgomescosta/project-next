71e5e33f1263c982d76b8338ba073f1f9c4c6669
lucas gomes <lucas.costa@sefaz.rr.gov.br>
2021-07-16 11:21:49 -0400

    configurando storybook e jest

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from "styled-components"
-import { theme } from "../styles/theme"
+/* eslint-disable prettier/prettier */
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }) {
   return (
-  <ThemeProvider theme={theme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
-  )
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+MyApp.propTypes = AppProps;
